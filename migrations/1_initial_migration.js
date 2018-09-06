48d4726deb4ebab1bb39b095b6b44b3f7489622d
yace132 <10000reset@gmail.com>
2018-09-06 15:30:47 +0800

    disable truffle migration contract
    insuffient money error ( although money is enough )

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,5 +2,5 @@ var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
   console.log("migrate contract")
-  deployer.deploy(Migrations);
+  //deployer.deploy(Migrations,{gas:300000});
 };
